c2bc83ec62a3737fe63f3dcc3cf81f092ed38c7a
Siddharth Chopra <sid6327@gmail.com>
2018-01-06 20:31:21 +0530

    Adding special characters without library.

--- LCD-CUSTOM/LCD-CUSTOM.ino
+++ LCD-CUSTOM/LCD-CUSTOM.ino
@@ -8,14 +8,11 @@ void sendOut(unsigned int data) {
 	for (int i = 0; i < 8; i++) {
 		if (mask & data) {
 			digitalWrite(D[i], HIGH);
-			Serial.print('1');
 		} else {
 			digitalWrite(D[i], LOW);
-			Serial.print('0');
 		}
 		mask = (mask << 1);
 	}
-	Serial.println('x');
 	delay(5);
   	digitalWrite(lcdE, HIGH);
   	delay(5);
@@ -37,6 +34,24 @@ void lcd_data(unsigned int data) {
 	sendOut(data);
 }
 
+void initLCD() {
+	// Init LCD
+	delay(20);			// LCD Power ON delay always > 15ms 
+	lcd_cmd(0x38);		// Initialization of 16X2 LCD in 8bit mode,  
+	lcd_cmd(0x0C);		// Display ON Cursor OFF 
+	lcd_cmd(0x06);		// Auto Increment cursor 
+	lcd_cmd(0x01);		// Clear display 
+	delay(2);			// Clear display command delay > 1.63 ms 
+	lcd_cmd(0x80);		// Cursor at home position
+}
+
+void customChar(int location, char charMatrix[]) {
+	lcd_cmd(0x40 + (location * 8));	// Start storing data at this location
+	for (int i = 0; i < 8; i++) {
+		lcd_data(charMatrix[i]);	
+	}
+}
+
 void setup() {
 	Serial.begin(9600);
 	pinMode(lcdE, OUTPUT);
@@ -46,30 +61,57 @@ void setup() {
 		pinMode(D[i], OUTPUT);
 	}
 
-	// Init LCD
-	delay(20);			// LCD Power ON delay always > 15ms 
-	lcd_cmd(0x38);		// Initialization of 16X2 LCD in 8bit mode,  
-	lcd_cmd(0x0C);		// Display ON Cursor OFF 
-	lcd_cmd(0x06);		// Auto Increment cursor 
-	lcd_cmd(0x01);		// clear display 
-	delay(2);			// Clear display command delay > 1.63 ms 
-	lcd_cmd(0x80);
+	initLCD();
 	
-	// Send data
-	lcd_cmd(0x40);	// Start storing data at this location
-	lcd_data(0x04);	
-	lcd_data(0x0e);
-	lcd_data(0x0e);
-	lcd_data(0x0e);
-	lcd_data(0x1f);
-	lcd_data(0x00);
-	lcd_data(0x04);
-	lcd_data(0x00);
+	char charMatrix[][8] = { { 0x00, 0x0A, 0x15, 0x11, 0x0A, 0x04, 0x00, 0x00 },                                
+							 { 0x04, 0x1F, 0x11, 0x11, 0x1F, 0x1F, 0x1F, 0x1F },  
+							 { 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x00, 0x04, 0x00 },
+							 // { 0x01, 0x03, 0x07, 0x1F, 0x1F, 0x07, 0x03, 0x01 },
+							 // { 0x01, 0x03, 0x05, 0x09, 0x09, 0x0B, 0x1B, 0x1 },
+							 { 0x0A, 0x0A, 0x1F, 0x11, 0x11, 0x0E, 0x04, 0x04 },
+							 { 0x00, 0x00, 0x0A, 0x00, 0x04, 0x11, 0x0E, 0x00 },
+							 { 0x00, 0x0A, 0x1F, 0x1F, 0x0E, 0x04, 0x00, 0x00 } };
 
-	lcd_cmd(0xc0);	// Get to second line
-	lcd_data(0x00);	// Display data from this location
+	char car0[8] = {
+		0b00000,
+		0b00111,
+		0b00100,
+		0b01100,
+		0b11111,
+		0b10011,
+		0b01101,
+		0b01100
+	};
+
+	char car1[8] = {
+		0b00000,
+		0b11000,
+		0b01000,
+		0b01000,
+		0b11111,
+		0b11001,
+		0b10110,
+		0b00110
+	};
+
+	for (int i = 0; i < 6; i++) {
+		customChar(i, charMatrix[i]);
+	}
+
+	customChar(6, car0);
+	customChar(7, car1);
+
+	// Send data
+	lcd_cmd(0x80);	// Get to first line
+	for (int i = 0; i < 6; i++) {
+		lcd_data(i);	// Display data from this location
+		lcd_data(' ');
+	}
+
+	lcd_data(6);
+	lcd_data(7);
 }
 
 void loop() {
-
+	
 }
